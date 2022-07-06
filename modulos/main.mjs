b9f45af75c90df381289bf667fd63fa55c416238
GabrielVithor <gabrielvithorcv@hotmail.com>
2022-07-06 02:37:52 -0300

    modulos-1.0

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade,mostraIdade,mostraHobby } from "./script.mjs";
+
+console.log(mostraIdade('gabriel',23))
+console.log(mostraCidade('gabriel',"Mauá"))
+console.log(mostraHobby('gabriel',"Programar"))
